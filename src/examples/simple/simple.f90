26d4dc0a4d9ba3cc15e9a45c7b9d058772ff69cc
Ondrej Certik <ondrej@certik.cz>
2009-12-16 14:40:06 -0800

    expose the solution

--- src/examples/simple/simple.f90
+++ src/examples/simple/simple.f90
@@ -4,16 +4,23 @@ print*, "Hello from Fortran!"
 print*, "a=",a
 end
 
-subroutine run()
+subroutine run(n, x, y, sol)
 use phaml
 !----------------------------------------------------
 
 implicit none
 
+integer n
+double precision x(n), y(n), sol(n)
+!f2py intent(in) x
+!f2py intent(in) y
+!f2py intent(out) sol
 !----------------------------------------------------
 ! Local variables
 
 type(phaml_solution_type) :: soln
+!real(my_real), allocatable :: x(:), y(:), u(:)
+
 !----------------------------------------------------
 ! Begin executable code
 
@@ -30,6 +37,20 @@ call phaml_solve_pde(soln,                   &
                      print_error_what=ENERGY_LINF_ERR, &
                      print_errest_what=ENERGY_LINF_ERREST, &
                      print_error_who=MASTER)
+
+!allocate(x(3), y(3), u(3))
+!x(1) = 0
+!x(2) = 0.5
+!x(3) = 1
+!y(1) = 0
+!y(2) = 0.5
+!y(3) = 1
+
+call phaml_evaluate(soln, x, y, sol)
+
+print*, "x=", x
+print*, "y=", y
+print*, "u=", sol
 
 call phaml_destroy(soln)
 end
--- src/examples/simple/t.py
+++ src/examples/simple/t.py
@@ -1,2 +1,10 @@
+from numpy import array
+
 import simple
-simple.run()
+x = array([0, 0.6, 1])
+y = array([0, 0.5, 1])
+#sol = array([0, 0.5, 1])
+u = simple.run(x, y)
+print u
+print type(u)
+print u.dtype
