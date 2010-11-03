5a2bab4292a2b86509a56524428ba4fbac6c604d
Ondrej Certik <ondrej@certik.cz>
2010-11-02 21:22:38 -0700

    get_solution_values() added

--- examples/simple.py
+++ examples/simple.py
@@ -11,4 +11,10 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 domain_file = os.path.join(current_dir, "domain")
 p = Phaml(domain_file)
 p.solve()
-print p.get_mesh()
+x, y, elems, orders = p.get_mesh()
+for n, elem in enumerate(elems):
+    print "Element #%d: %s, order=%d" % (n, elem, orders[n])
+    for i in elem:
+        print "   ", x[i], y[i]
+values = p.get_solution_values(x, y)
+print values[:10]
--- phaml/simple_wrapper.pyx
+++ phaml/simple_wrapper.pyx
@@ -25,3 +25,13 @@ cdef class Phaml(object):
         simple.c_phaml_get_mesh(&n, &xvert[0], &yvert[0],
             &nelem, &element_vertices[0, 0], &orders[0])
         return xvert, yvert, element_vertices, orders
+
+    def get_solution_values(self,
+            ndarray[double, mode="c"] x not None,
+            ndarray[double, mode="c"] y not None):
+        cdef int n = len(x)
+        assert len(y) == n
+        cdef ndarray[double, mode="c"] values = empty(n, dtype="double")
+        simple.c_phaml_get_solution_values(&n, &x[0], &y[0], &values[0])
+        return values
+
