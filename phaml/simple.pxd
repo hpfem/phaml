e8815c30dd73141a8e36dcd8e2ee3139e71ef780
Ondrej Certik <ondrej@certik.cz>
2010-11-02 17:28:10 -0700

    Update the Python wrappers

--- phaml/simple.pxd
+++ phaml/simple.pxd
@@ -1,7 +1,7 @@
 cdef extern void c_phaml_init(char *triangle_files, int *triangle_files_len)
 cdef extern void c_phaml_solve()
 cdef extern void c_phaml_get_mesh_info(int *n, int *nelem)
-cdef extern void c_phaml_get_mesh(int n, double *xvert, double *yvert,
+cdef extern void c_phaml_get_mesh(int *n, double *xvert, double *yvert,
         int *nelem, int *element_vertices, int *orders)
 cdef extern void c_phaml_get_solution_values(int *n, double *x, double *y,
         double *values)
--- phaml/simple_wrapper.pyx
+++ phaml/simple_wrapper.pyx
@@ -3,15 +3,23 @@ from numpy import empty
 
 cimport simple
 
-def run(ndarray[double, mode="c"] x not None,
-        ndarray[double, mode="c"] y not None, triangle_files):
-    """
-    Calls c_run() from simple.f90
-    """
-    cdef int n = len(x)
-    assert len(y) == n
-    cdef ndarray[double, mode="c"] sol = empty(n)
-    cdef char *s = triangle_files
-    cdef int triangle_files_len = len(triangle_files)
-    simple.c_run(&n, &x[0], &y[0], &sol[0], s, &triangle_files_len)
-    return sol
+cdef class Phaml(object):
+
+    def __init__(self, triangle_files):
+        cdef char *s = triangle_files
+        cdef int triangle_files_len = len(triangle_files)
+        simple.c_phaml_init(s, &triangle_files_len)
+
+    def solve(self):
+        simple.c_phaml_solve()
+
+    def get_mesh(self):
+        cdef int n, nelem
+        simple.c_phaml_get_mesh_info(&n, &nelem)
+        cdef ndarray[double, mode="c"] xvert = empty(n, dtype="double")
+        cdef ndarray[double, mode="c"] yvert = empty(n, dtype="double")
+        cdef ndarray[int, mode="c"] element_vertices = empty(nelem, dtype="int")
+        cdef ndarray[int, mode="c"] orders = empty(nelem, dtype="int")
+        simple.c_phaml_get_mesh(&n, &xvert[0], &yvert[0],
+            &nelem, &element_vertices[0], &orders[0])
+        return xvert, yvert, element_vertices, orders
